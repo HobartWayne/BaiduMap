1b7b02fd9ba63626d99420732e94c5ec0c6538bc
HobartWayne <hb_weng@163.com>
2016-10-26 09:14:41 +0800

    second commit

---
+++ app/src/main/java/com/hb_weng/baidumap/Enemy.java
@@ -0,0 +1,15 @@
+package com.hb_weng.baidumap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hb_weng.hellobaidumap.R;
+
+public class Enemy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enemy);
+    }
+}
